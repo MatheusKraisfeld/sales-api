22b7ff409acb775faf0863ce4ab612fa6a15a447
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2021-08-19 20:04:16 -0300

    Feat: Cancel sales order

--- src/main/java/com/github/matheuskraisfeld/domain/repository/ItensPedido.java
+++
@@ -1,7 +0,0 @@
-package com.github.matheuskraisfeld.domain.repository;
-
-import com.github.matheuskraisfeld.domain.entity.ItemPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItensPedido extends JpaRepository<ItemPedido, Integer> {
-}
