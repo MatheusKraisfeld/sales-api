dee5ad5a3fcbbfb0c79363b1d08a27117da8ca99
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2021-08-20 19:47:19 -0300

    Refactor: Bean validation

---
+++ src/main/java/com/github/matheuskraisfeld/validation/constraintvalidation/NotEmptyListValidator.java
@@ -0,0 +1,20 @@
+package com.github.matheuskraisfeld.validation.constraintvalidation;
+
+import com.github.matheuskraisfeld.validation.NotEmptyList;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class NotEmptyListValidator
+        implements ConstraintValidator<NotEmptyList, List> {
+    @Override
+    public void initialize(NotEmptyList constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List value, ConstraintValidatorContext context) {
+        return value != null && !value.isEmpty();
+    }
+}
