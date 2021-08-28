85033138e7effc130a0169bdc60a0b3c4dede882
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2021-08-28 20:35:11 -0300

    Feat: JWT Auth in Swagger UI

--- src/main/java/com/github/matheuskraisfeld/config/SwaggerConfig.java
+++ src/main/java/com/github/matheuskraisfeld/config/SwaggerConfig.java
@@ -1,14 +1,19 @@
 package com.github.matheuskraisfeld.config;
 
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -22,6 +27,8 @@ public class SwaggerConfig {
                         .basePackage("com.github.matheuskraisfeld.rest.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .apiInfo(apiInfo());
     }
 
@@ -39,6 +46,28 @@ public class SwaggerConfig {
                 "Matheus Kraisfeld",
                 "https://github.com/matheuskraisfeld",
                 "matheuskraisfeld@gmail.com");
+    }
+
+    public ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(reference);
+        return auths;
     }
 
 }
